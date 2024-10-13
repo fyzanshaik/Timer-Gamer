bbfdbe398cbca46ebe027a5d744a71077cf7b2f2
fyzanshaik <fyzan.shaik@gmail.com>
2024-10-14 01:02:41 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import Player from './components/Player.jsx';
 import TimerChallenge from './components/TimerChallenge.jsx';
 import { Header } from './components/Header.jsx';
+
 function App() {
   return (
     <>
       <Header />
       <Player />
       <div id="challenges">
-        <TimerChallenge title="Easy" targetTime={1} ></TimerChallenge>
-        {/* <TimerChallenge title="Not Easy" targetTime={5} ></TimerChallenge>
-        <TimerChallenge title="Not Easy" targetTime={5} ></TimerChallenge>
-
-        <TimerChallenge title="Not Easy" targetTime={5} ></TimerChallenge> */}
+        <TimerChallenge title="Quick Reflex" targetTime={1} />
+        <TimerChallenge title="Speed Test" targetTime={3} />
+        <TimerChallenge title="Endurance" targetTime={10} />
+        <TimerChallenge title="Focus Mode" targetTime={15} />
+        <TimerChallenge title="Ultimate Challenge" targetTime={30} />
       </div>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 export const Header = () => {
-	return (
-		<header>
-			'React' refers to a UMD global, but the current file is a module. Consider adding an import instead.ts(2686){' '}
-			<h1>
-				The <em>Almost</em> Final Countdown
-			</h1>
-			<p>Stop the timer once you estimate that time is (almost) up</p>
-		</header>
-	);
+    return (
+        <header>
+            <h1>
+                The <em>Almost</em> Final Countdown
+            </h1>
+            <p>Stop the timer once you estimate that time is (almost) up</p>
+        </header>
+    );
 };
