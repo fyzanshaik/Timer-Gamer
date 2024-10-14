ce36f1972a6f49cb3b7de86c1db9b5b42fa43b1c
fyzanshaik <fyzan.shaik@gmail.com>
2024-10-15 03:46:57 +0530

    key

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const corsOptions = {
-	origin: 'http://localhost:5173',
+	origin: [
+		'https://timer-gamer.vercel.app',
+		'https://timer-gamer-nn3lfzkme-fyzanshaiks-projects.vercel.app',
+		'https://vercel.live/link/timer-gamer-git-main-fyzanshaiks-projects.vercel.app?via=project-dashboard-alias-list&p=1',
+		'https://timer-gamer-fyzanshaiks-projects.vercel.app/',
+	],
 	methods: ['GET', 'POST', 'PUT', 'DELETE'],
 	allowedHeaders: ['Content-Type', 'Authorization'],
 	credentials: true,
