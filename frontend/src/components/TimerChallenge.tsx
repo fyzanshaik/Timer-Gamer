42c9f28a443b3cf1db450e0c435cdcad043431b2
fyzanshaik <fyzan.shaik@gmail.com>
2024-10-15 04:10:34 +0530

    New state in timer challenge

--- frontend/src/components/TimerChallenge.tsx
+++ frontend/src/components/TimerChallenge.tsx
@@ -1,17 +1,22 @@
 import { useRef, useState, useCallback, useEffect } from 'react';
 import { ResultModal } from './ResultModal';
 import { TimerChallengeProps } from '../interfaces';
-import Leaderboard, { LeaderboardHandle } from './Leaderboard'; // Import the Leaderboard with Handle
+import Leaderboard, { LeaderboardHandle } from './Leaderboard';
 
 const TimerChallenge: React.FC<TimerChallengeProps> = ({ title, targetTime, highScore, userId }) => {
 	const [remainingTime, setRemainingTime] = useState<number>(targetTime * 1000);
 	const [userHighscore, setUserHighscore] = useState<number>(highScore);
+	const [evaluatedHighScore, setEvaluatedHighScore] = useState<number>(highScore);
 
 	const timerRef = useRef<ReturnType<typeof setInterval> | null>(null);
 	const dialog = useRef<{ open: () => void }>(null);
 	const leaderDialog = useRef<LeaderboardHandle>(null);
 
 	const timerIsActive = remainingTime > 0 && remainingTime < targetTime * 1000;
+
+	useEffect(() => {
+		setEvaluatedHighScore(userHighscore > highScore ? userHighscore : highScore);
+	}, [userHighscore, highScore]);
 
 	useEffect(() => {
 		if (remainingTime <= 0 && timerRef.current) {
@@ -50,11 +55,11 @@ const TimerChallenge: React.FC<TimerChallengeProps> = ({ title, targetTime, high
 	return (
 		<>
 			<ResultModal ref={dialog} userId={userId} targetTime={targetTime} remainingTime={remainingTime} handleReset={handleReset} setUserHighScore={setUserHighscore} userHighScore={userHighscore} />
-			{userId == 1 ? null : <Leaderboard ref={leaderDialog} timerKey={`timer${targetTime}`} highScore={userHighscore} />}
+			{userId == 1 ? null : <Leaderboard ref={leaderDialog} timerKey={`timer${targetTime}`} highScore={evaluatedHighScore} />}
 			<section className="challenge">
 				<h2>{title}</h2>
 				<p>
-					<strong>Your highscore: {userHighscore > highScore ? userHighscore : highScore}</strong>
+					<strong>Your highscore: {evaluatedHighScore}</strong>
 				</p>
 				<p className="challenge-time">
 					{targetTime} second{targetTime > 1 ? 's' : ''}
